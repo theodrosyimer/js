21ac6be7d7a6cc9c161b5bbabf1561afc457d238
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-12-10 18:32:35 +0100

    refactor: refactor curry function and add console.log

--- curry-example.js
+++ curry-example.js
@@ -1,7 +1,9 @@
 // source: [How to Curry Functions | An Advanced Javascript Tutorial on Currying - (Youtube)](https://www.youtube.com/watch?v=I4MebkHvj8g&list=PL0Zuz27SZ-6N3bG4YZhkrCL3ZmDcLTuGd&index=5&ab_channel=DaveGray)
-const curry = (fn) => {
+function curry(fn) {
   let curried
   return (curried = (...args) => {
+    console.log('args', args)
+
     if (fn.length !== args.length) {
       return curried.bind(null, ...args)
     }
@@ -12,3 +14,8 @@ const curry = (fn) => {
 // example
 const total = (x, y, z) => x + y + z
 const curriedTotal = curry(total)
+
+curriedTotal(1)(2)(3)
+// curriedTotal(1, 2)(3)
+// curriedTotal(1)(2, 3)
+// curriedTotal(1, 2, 3)
