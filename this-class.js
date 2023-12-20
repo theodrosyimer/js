0eb27764686e65d6a0a28f8ac37e23b43b2a1317
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-12-20 16:26:38 +0100

    reactor: refactor this-bind-function.js and this-class.js

--- this-class.js
+++ this-class.js
@@ -1,18 +1,24 @@
 /* eslint-disable prefer-destructuring */
+// source: [this Demystified // Professional JavaScript - (Youtube)](https://www.youtube.com/watch?v=X-Q22LkJhs8&t=101s&ab_channel=basarat)
 
 // check ./class.js
-// ES7 syntax
-// Always use ES7 syntax vs ES6 syntax
+
+// ES7 syntax vs ES6 syntax
 class Person {
   // private _age: number
   constructor(_age /* : number */) {
     this._age = _age
   }
 
+  // slight difference of performance vs using a method (better) and an instance property
+  // Pros: `this` is captured correctly with an instance property using an arrow function
+  // Cons: an instance property is not shared across all instances of the class
   growOld = () => {
     this._age++
   }
 
+  // Pros: a method is shared across all instances of the class
+  // Cons: needs to be called correctly! see below
   age() {
     return this._age
   }
