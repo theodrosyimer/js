5b0c7c9cb11a7b354cfb91c39162a6a5a743ef13
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-12-20 16:28:06 +0100

    refactor: refactor URL.canParse tests

--- url-canParse.js
+++ url-canParse.js
@@ -1,18 +1,21 @@
-if ("canParse" in URL) {
-  console.log("Test valid absolute URL")
-  let url = "https://developer.mozilla.org/"
-  let result = URL.canParse(url)
-  console.log(` URL.canParse("${url}"): ${result}`)
+if ('canParse' in URL) {
+  console.log('Test valid absolute URL')
+  let url = 'https://developer.mozilla.org/'
+  console.log(` URL.canParse("${url}"): ${URL.canParse(url)}`)
 
-  console.log("\nTest relative URL with no base URL")
-  url = "/en-US/docs"
-  result = URL.canParse(url)
-  console.log(` URL.canParse("${url}"): ${result}`)
+  console.log('\nTest relative URL with no base URL')
+  url = '/en-US/docs'
+  console.log(` URL.canParse("${url}"): ${URL.canParse(url)}`)
 
-  console.log("\nTest relative URL with valid base URL")
-  let baseUrl = "https://developer.mozilla.org/"
-  result = URL.canParse(url, baseUrl)
-  console.log(` URL.canParse("${url}","${baseUrl}"): ${result}`)
+  console.log('\nTest relative URL with valid base URL')
+  console.log(
+    ` URL.canParse("${url}","${baseUrl}"): ${URL.canParse(
+      url,
+      'https://developer.mozilla.org/',
+    )}`,
+  )
 } else {
-  console.log("Can't validated passed url because of URL.canParse() being not supported")
+  console.log(
+    "Can't validated passed url because of URL.canParse() being not supported",
+  )
 }
