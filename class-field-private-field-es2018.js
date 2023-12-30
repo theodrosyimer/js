4eb610b1b20d2344d13e0025fed52bfe377701e9
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-12-30 21:37:25 +0100

    feat: add class with private and public fields and methods

---
+++ class-field-private-field-es2018.js
@@ -0,0 +1,33 @@
+class IncreasingCounter {
+  // private field
+  #count = 0
+
+  // public field
+  person = 'Theo'
+
+  get value() {
+    console.log('Getting the current value!')
+    return `${this.person}: ${this.#count}`
+  }
+
+  increment() {
+    this.#count++
+  }
+}
+
+const counter = new IncreasingCounter()
+
+// Accessing a public field
+console.log(counter.person) // ?
+
+// We can't acces the private field #count, it returns undefined
+console.log(counter.count) // ?
+
+// Accessing a public method
+console.log(counter.value) // ?
+
+// Trying to access the private field `#count` value via the public method ìncrement()`
+console.log(counter.increment()) // ?
+
+// Accessing the new value of `counter.value` after incrementing it
+console.log(counter.value) // ?
