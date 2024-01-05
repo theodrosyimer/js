4c9401cda054752a820c9663545aee98ae197194
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-05 17:42:20 +0100

    feat : add classes with private and public fields and methods

--- class-field-private-field.js
+++ class-field-private-field.js
@@ -31,3 +31,27 @@ console.log(counter.increment()) // ?
 
 // Accessing the new value of `counter.value` after incrementing it
 console.log(counter.value) // ?
+
+///////////////////////////////////////////////////////////////////
+
+class Person {
+  #firstName
+  #lastName
+  constructor(firstName, lastName) {
+    this.#firstName = firstName
+    this.#lastName = lastName
+  }
+  getFullName(format = true) {
+    return format ? this.#firstLast() : this.#lastFirst()
+  }
+
+  #firstLast() {
+    return `${this.#firstName} ${this.#lastName}`
+  }
+  #lastFirst() {
+    return `${this.#lastName}, ${this.#firstName}`
+  }
+}
+
+let person = new Person('John', 'Doe')
+console.log(person.getFullName())
