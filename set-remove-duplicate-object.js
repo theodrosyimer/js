bb151fabd973f1efb4ef2a081f7fe90bd5cac85b
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-03-17 12:58:02 +0100

    remove duplicate objects

--- set-remove-duplicate-object.js
+++ set-remove-duplicate-object.js
@@ -1,39 +1,43 @@
-let o = {
+const o = {
   name: 'gentilly',
   lat: 48,
-  lon: 33
+  lon: 33,
 }
 
-let o1 = {
+const o1 = {
   name: 'gentilly',
   lat: 48,
-  lon: 33
+  lon: 33,
 }
 
-let o2 = {
+const object2 = {
   name: 'paris',
   lat: 47,
-  lon: 32
+  lon: 32,
 }
 
-// let set = new Set(Object.entries(o), Object.entries(o1), Object.entries(o2))
+const set = new Set(
+  Object.entries(o),
+  Object.entries(o1),
+  Object.entries(object2),
+)
 
-// Object.fromEntries(set)   // ?
+Object.fromEntries(set) // ?
 
-function removeDuplicatesObjects(...objects) {
-  let myMap = new Map()
-  let lastObjectMap = new Map(Object.entries(objects.pop()))
-  objects.forEach(object => {
-    myMap.set(Object.entries(object))
-    for (const [key, value] of myMap.entries()) {
-      if (lastObjectMap.get(key)) {
-        myMap
-      }
-    }
-  })
-  // const result = Object.fromEntries(
-  //   Object.entries(object3).filter(([key, value]) => key.length === 1)
-  // ) // ?
-}
+// function removeDuplicatesObjects(...objects) {
+//   const myMap = new Map()
+//   const lastObjectMap = new Map(Object.entries(objects.pop()))
+//   objects.forEach(object => {
+//     myMap.set(Object.entries(object))
+//     for (const [key, value] of myMap.entries()) {
+//       if (lastObjectMap.get(key)) {
+//         myMap
+//       }
+//     }
+//   })
+//   // const result = Object.fromEntries(
+//   //   Object.entries(object3).filter(([key, value]) => key.length === 1)
+//   // ) // ?
+// }
 
-removeDuplicatesObjects(o, o1, o2) // ?
+// removeDuplicatesObjects(o, o1, object2) // ?
