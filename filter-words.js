2bc49f198ce82db7ef9999213fb4a88817678ee7
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-03-17 12:43:02 +0100

    Add filterWords function

--- filter-words.js
+++ filter-words.js
@@ -5,18 +5,14 @@ filterWords(wordsArr, lettersStr)
 
 function filterWords(words, letters) {
   const res = []
-  let iterationCount = 0
   for (let index = 0; index < words.length; index++) {
     const currentWord = words[index]
 
     for (let j = 0; j < letters.length; j++) {
-      iterationCount++
       if (currentWord.includes(letters[j])) {
         res.push(currentWord)
       }
     }
   }
-  console.log(iterationCount)
   return res
 }
-
