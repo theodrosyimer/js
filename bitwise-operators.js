01b329798f08fa67e56f13e4adc7a1e512bfef23
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-11-03 03:19:52 +0100

    feat: add bitwise operators examples in a new file

---
+++ bitwise-operators.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable eqeqeq */
+/* eslint-disable no-bitwise */
+/* eslint-disable no-unused-expressions */
+
+// Bitwise operators
+// <<
+// >>
+// &
+// |
+// ~
+// ^
+
+let a = 3 << 2 // ?
+
+//    0011
+// -> 1100
+
+a = 9 >> 2 // ?
+
+//    1001
+// -> 0010
+
+a = 9 >>> 2 // ?
+
+//    1001
+// -> 0010
+
+a = 7 & 2 // ?
+
+//    0111
+//    0010
+// -> 0010
+
+a = 7 | 1 // ?
+
+//    0111
+//    0001
+// -> 0111
+
+a = 7 ^ 1 // ?
+
+//    0111
+//    0001
+// -> 0110
+
+a = ~1 //  ?
+// a = ~x = -x - 1
+// a = ~1 = -1 - 1 = 1111 1110
