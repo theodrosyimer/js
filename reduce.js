8eaacc256c47168c10e4c5a1eb3d8d03f2282616
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-11-03 03:22:25 +0100

    style: add missing comments for clarity in reduce examples

--- reduce.js
+++ reduce.js
@@ -12,27 +12,27 @@ const persons = [
 // this is bad because it creates a new object on each iteration
 const ob = persons.reduce(
   (acc, person) => ({ ...acc, [person.id]: person }),
-  {}
-) //?.
+  {},
+) // ?.
 ob
 
 // this is good because it only creates a new object once (initialValue)
 const ob2 = persons.reduce((hashTable, person) => {
   hashTable[person.id] = person
   return hashTable
-}, {}) //?.
+}, {}) // ?.
 ob2
 
 // ARRAYS
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 
 // this is bad because it creates a new array on each iteration
-const arr = numbers.reduce((acc, num) => [...acc, num * 2], []) //?.
+const arr = numbers.reduce((acc, num) => [...acc, num * 2], []) // ?.
 arr
 
 // this is good because it only creates a new array once (initialValue)
 const arr2 = numbers.reduce((acc, num) => {
   acc.push(num * 2)
   return acc
-}, []) //?.
+}, []) // ?.
 arr2
