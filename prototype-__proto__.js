617c0376fbc80915a751b63006d5919b54c1aa1f
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-11-03 03:21:57 +0100

    refactor: enhance logging for prototype chain examples

--- prototype-__proto__.js
+++ prototype-__proto__.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-proto */
 // .__proto__ === [[Prototype]] (a hidden property in objects)
 
 /**
@@ -5,28 +6,41 @@
  */
 const alpha = { name: 'Alpha' }
 
-console.log(typeof alpha.name)
+console.log('[typeof alpha.name]:', typeof alpha.name)
 
 //  if `alpha` doesn't have a `toString` method,
 // it will look for it in `alpha.__proto__`
 // and if it doesn't find it there, it will look for it in `alpha.__proto__.__proto__`
 // and so on until it reaches the end of the prototype chain
-console.log(typeof alpha.toString)
+console.log('[typeof alpha.toString]:', typeof alpha.toString)
 
 // `__proto__` is an object
-console.log(typeof alpha.__proto__)
+console.log('[typeof alpha.__proto__]:', typeof alpha.__proto__)
+// better to access `__proto__` using `Object.getPrototypeOf`
+console.log(
+  '[typeof Object.getPrototypeOf(alpha)]:',
+  typeof Object.getPrototypeOf(alpha),
+)
 
 // `__proto__` is an object that has a `toString` method
-console.log(typeof alpha.__proto__.toString)
+console.log(
+  '[typeof alpha.__proto__.toString]:',
+  typeof alpha.__proto__.toString,
+)
+// better to access `__proto__` using `Object.getPrototypeOf`
+console.log(
+  '[typeof Object.getPrototypeOf(alpha).toString()]:',
+  typeof Object.getPrototypeOf(alpha).toString(),
+)
 
 // example of a prototype chain by using `__proto__`
 const beta = { age: 10, __proto__: alpha }
 
-console.log(beta.age)
+console.log('[beta.age]:', beta.age)
 console.log('beta.name from `alpha`:', beta.name)
 
 alpha.name = 'Alpha 2'
-console.log('Editing alpha.name changes `beata.name`:',beta.name)
+console.log('Editing alpha.name changes `beata.name`:', beta.name)
 
 /**
  * ### Example 2
@@ -38,17 +52,17 @@ const person = {
       __proto__: {
         name: 'Alpha',
         age: 10,
-      }
-    }
-  }
+      },
+    },
+  },
 }
 
-console.log(person.name)
-console.log(person.age)
-console.log(person.weight)
+console.log('[person.name]:', person.name)
+console.log('[person.age]:', person.age)
+console.log('[person.weight]:', person.weight)
 
 // this is what JavaScript does behind the scenes to find `weight` property in this example:
-console.log(person.__proto__.__proto__.__proto__.__proto__.__proto__ )
+console.log(person.__proto__.__proto__.__proto__.__proto__.__proto__)
 
 // `__proto__` returns finally a `null` value so JavaScript returns `undefined`
 console.log(person.__proto__.__proto__.__proto__.__proto__.__proto__ === null)
